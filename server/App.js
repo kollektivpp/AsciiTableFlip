8c33e57513c6670d5ff127829db01af387890985
Gernot Labahn <gla@sinnerschrader-mobile.com>
2015-11-15 12:14:03 +0100

    Add possibility to control server port via command line arg

--- server/App.js
+++ server/App.js
@@ -2,17 +2,21 @@
 
 var app = require('express')(),
     server = require('http').Server(app),
-    io = require('socket.io')(server);
+    io = require('socket.io')(server),
+    port = process.env.npm_config_port || 8080;
 
 app.get('/', function(req, res) {
-    console.log(__dirname);
     res.sendFile(__dirname + '/index.html');
+});
+
+app.get('/client', function(req, res) {
+    res.sendFile(__dirname + './../client/index.html');
 });
 
 io.on('connection', function(socket){
     console.log('a user connected');
 });
 
-server.listen(8080, function() {
-    console.log('listening on *:8080');
+server.listen(port, function() {
+    console.log('listening on *:' + port);
 });
